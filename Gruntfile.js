4bcd0bf0a66c3995292491c9d61445688b1bd31c
ashjd <ashjd29@gmail.com>
2016-05-19 11:10:06 -0700

    add git push to origin

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,7 +66,7 @@ module.exports = function(grunt) {
 
     shell: {
       prodServer: {
-        command: 'git push live master'
+        command: 'git push live master && git push origin master'
       }
     },
   });
