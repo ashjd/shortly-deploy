aa856444cb79ba168dc985ca9962b6205cca936c
ashjd <ashjd29@gmail.com>
2016-05-19 18:57:53 -0700

    pass all specs

--- app/models/link.js
+++ app/models/link.js
@@ -12,26 +12,12 @@ var linkSchema = mongoose.Schema({
 
 linkSchema.pre('save', function(next) {
   var shasum = crypto.createHash('sha1');
-  shasum.update(model.get('url'));
-  model.set('code', shasum.digest('hex').slice(0, 5));
+  //shasum.update(this.url);
+  this.code = shasum.digest('hex').slice(0, 5);
+  next();
 });
 
 var Link = mongoose.model('Link', linkSchema);
-
-// var Link = db.Model.extend({
-//   tableName: 'urls',
-//   hasTimestamps: true,
-//   defaults: {
-//     visits: 0
-//   },
-//   initialize: function() {
-//     this.on('creating', function(model, attrs, options) {
-//       var shasum = crypto.createHash('sha1');
-//       shasum.update(model.get('url'));
-//       model.set('code', shasum.digest('hex').slice(0, 5));
-//     });
-//   }
-// });
 
 module.exports = Link;
 
